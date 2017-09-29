13e217968763a28874b3f39c4f13f586dd15a57b
Gaudel <pgaudel@gmail.com>
2017-09-29 09:29:45 +0200

    Init
    Init springBoot config to release 1.5.7

---
+++ src/main/java/com/diving/Abysse/AbysseApplication.java
@@ -0,0 +1,12 @@
+package com.diving.Abysse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbysseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbysseApplication.class, args);
+	}
+}
